5c66aadef16fe13c9d38bbe5f26c92a5bad6f813
Denys Lins <denyslins@gmail.com>
2019-01-10 21:16:15 -0300

    Feature Normalization

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,13 +26,16 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
-
-
-
-
+for i = 1 : size(X, 2)
+  
+  mu(1, i) = mean(X(:,i));
+  sigma(1, i) = std(X(:,i));
+  
+  for j = 1 : size(X, 1)
+    X_norm(j, i) = ((X(j, i) - mu(1, i)) / sigma(1, i));
+  endfor
+      
+endfor
 
 % ============================================================
 
