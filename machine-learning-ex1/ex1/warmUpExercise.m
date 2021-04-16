a142166f81bd989053e7cc9de2185fc4002433b7
Denys Lins <denys.lins@hp.com>
2021-04-16 17:23:26 -0300

    Update warmUpExercise.m for matlab

--- machine-learning-ex1/ex1/warmUpExercise.m
+++ machine-learning-ex1/ex1/warmUpExercise.m
@@ -7,11 +7,9 @@ A = [];
 % Instructions: Return the 5x5 identity matrix 
 %               In octave, we return values by defining which variables
 %               represent the return values (at the top of the file)
-%               and then set them accordingly.
+%               and then set them accordingly. 
 
 A = eye(5);
-
 % ===========================================
-
 
 end
